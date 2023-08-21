9c26b1d9cc5a865f4d6765947d443c7b3c6bdd54
Abhishek Singh <singhabhishek111213@gmail.com>
2023-08-21 22:58:44 +0530

    FEAT: Started Booking implementation

---
+++ src/services/booking-service.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+
+const { BookingRepository } = require("../repositories");
+const { ServerConfig } = require("../config")
+const db = require("../models");
+const {StatusCodes} = require("http-status-codes");
+const AppError = require("../utils/errors/app-error")
+async function createBooking(data){
+    return new Promise((resolve, reject)=>{
+            const result = db.sequelize.transaction( async function bookingImpl(t){
+                const flight =  await axios.get(`http://localhost:3000/api/v1/flights/${data.flightId}`);
+                const flightData = flight.data.data;
+                if(data.noOfSeats > flightData.totalSeats){
+                    reject(new AppError('Not enough seats available', StatusCodes.BAD_REQUEST));
+                }
+                resolve(true);
+            });
+    });
+}
+
+module.exports = {
+    createBooking
+}
