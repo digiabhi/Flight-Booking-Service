21b72cdd96496d392a59db796c5408da3bc81bc0
Abhishek Singh <singhabhishek111213@gmail.com>
2023-08-31 10:55:26 +0530

    FEAT: Added cancel booking code

--- src/repositories/booking-repository.js
+++ src/repositories/booking-repository.js
@@ -2,7 +2,7 @@ const { StatusCodes } = require("http-status-codes");
 
 const { Booking } = require("../models");
 const CrudRepository = require("./crud-repository");
-
+const AppError = require("../utils/errors/app-error");
 class BookingRepository extends CrudRepository {
   constructor() {
     super(Booking);
